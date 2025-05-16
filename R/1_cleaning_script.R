edca097042c155ab55bf1f6b319a949be23fb34b
mudfishey <163457891+mudfishey@users.noreply.github.com>
2025-05-16 12:15:19 +0200

    Update 1_cleaning_script.R

--- R/1_cleaning_script.R
+++ R/1_cleaning_script.R
@@ -3,7 +3,7 @@
 # This script is used to clean the data from the National Malaria Indicator     #
 # Survey 2019/2020.                                                             # 
 #                                                                               #
-# Author : S. Zavala and Sumin hahahahah                                                           #
+# Author : S. Zavala and Sumin                                                           #
 # Date   : 2025-15-05                                                           #
 #                                                                               # 
 #################################################################################
